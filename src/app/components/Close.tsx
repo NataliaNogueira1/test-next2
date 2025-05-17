89d9f344f13f3a93e1bf7f2e5829b213e7bd8b47
NataliaNogueira1 <natalianogueir2006@gmail.com>
2025-05-16 21:53:27 -0300

    mudanças no classname

--- src/app/components/Close.tsx
+++ src/app/components/Close.tsx
@@ -1,9 +1,9 @@
 import { IoCloseOutline } from "react-icons/io5";
 
-export default function Close(){
-    return(
-        <div>
-            <IoCloseOutline className="w-9 h-9" />
-        </div>
-    );
+export default function Close() {
+  return (
+    <div>
+      <IoCloseOutline className="w-9 h-9" />
+    </div>
+  );
 }
